c5216c8c3398eac99bcf29e93ee0a7445ed31693
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-11-25 19:26:37 -0300

    feat: create base for Navigation component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+function Navigation() {
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Where in the world?</h1>
+      <div>
+        Dark Mode
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
