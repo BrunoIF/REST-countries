0e2cd479c68f3b56f0fae10433122e7a26797f70
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-11-26 19:51:51 -0300

    feat: create constants file

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const REGION_OPTIONS = [
+  'Filter by Region',
+  'Africa',
+  'Americas',
+  'Asia',
+  'Europe',
+  'Oceania',
+];
+
+export const DEFAULT_AMOUNT_COUNTRIES_TO_DISPLAY = 15;
