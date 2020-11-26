c6dc287d694b1d16d8b15322e06e57570febcd3c
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-11-26 17:02:22 -0300

    feat: add Icon support for LinkButton component

--- src/components/LinkButton/index.js
+++ src/components/LinkButton/index.js
@@ -3,12 +3,16 @@ import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
-function LinkButton({ to, text, className }) {
-
+function LinkButton({
+  to, text, className, Icon,
+}) {
   return (
     <div className={`${styles.buttonContainer} ${className}`}>
       <Link href={to}>
-        <a className={styles.button}>{text}</a>
+        <a className={styles.button}>
+          {!!Icon && (<span className={styles.icon}>{Icon}</span>)}
+          {text}
+        </a>
       </Link>
     </div>
   );
