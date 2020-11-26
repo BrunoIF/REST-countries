e521a7694e9f44356afb9c6e021395b1b3d431f3
BrunoIF <bruno.i.fernandes@hotmail.com>
2020-11-26 19:52:18 -0300

    refactor(SelectList): add support for onChange callback

--- src/components/SelectList/index.js
+++ src/components/SelectList/index.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronUp, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import cn from 'classnames';
 
 import styles from './styles.module.scss';
 
-function SelectList({ options }) {
+function SelectList({ options, onChange }) {
   const [selectedValue, setSelectedValue] = useState(options[0]);
   const [isOpen, setIsOpen] = useState(false);
   const [isResetOpen, setIsResetOpen] = useState(false);
 
-  const handleOption = (value) => {
+  const handleChange = (value) => {
     setSelectedValue(value);
     setIsOpen(false);
   };
 
+  const handleReset = () => {
+    setIsOpen(false);
+    setSelectedValue(options[0]);
+    onChange('');
+  };
+
   useEffect(() => {
     if (selectedValue !== options[0]) {
+      onChange(selectedValue);
       setIsResetOpen(true);
     } else {
       setIsResetOpen(false);
@@ -26,7 +33,7 @@ function SelectList({ options }) {
   return (
     <div className={styles.container}>
       {isResetOpen && (
-        <div className={styles.reset} onClick={() => setSelectedValue(options[0])}>
+        <div className={styles.reset} onClick={() => handleReset()}>
           &times;
         </div>
       )}
@@ -40,7 +47,7 @@ function SelectList({ options }) {
       </div>
       <div className={cn(styles.options, { [styles.open]: isOpen })}>
         {options.slice(1, options.length).map(option => (
-          <div className={styles.option} onClick={() => handleOption(option)}>{option}</div>
+          <div className={styles.option} onClick={() => handleChange(option)}>{option}</div>
         ))}
       </div>
     </div>
