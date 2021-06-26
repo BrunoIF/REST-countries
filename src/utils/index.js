48c533616bb784151b37f4b60140c812221d8778
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-06-26 17:01:07 -0300

    feat: add isMobile util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,12 @@
-export const formatNumber = population => population.toLocaleString();
+export const formatNumber = (number) => number.toLocaleString();
 
-export const getStringListOfAttr = (list, attr) => list.map(item => item[attr]).join(', ');
+export const getStringListOfAttr = (list, attr) =>
+  list.map((item) => item[attr]).join(", ");
+
+export const isMobile = () => {
+  if (!window) {
+    return false;
+  }
+
+  return window?.innerWidth <= 768 ?? false;
+};
