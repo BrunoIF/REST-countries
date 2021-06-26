f3b92bcfb87a21062eb54e369d289129b192c0e5
Bruno Irokawa Fernandes <bruno.fernandes@fiap.com.br>
2021-06-26 17:01:36 -0300

    feat: implement theme switcher for mobile

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-regular-svg-icons';
-import { faMoon as faMoonSolid } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faMoon as faMoonSolid } from "@fortawesome/free-solid-svg-icons";
 
-import { useReactiveVar } from '@apollo/client';
-import { toggleTheme, isDarkThemeVar } from '../../lib/apolloStates';
+import { useReactiveVar } from "@apollo/client";
+import { toggleTheme, isDarkThemeVar } from "lib/apolloStates";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
+import { isMobile } from "utils";
 
 function Navigation() {
   const isDarkTheme = useReactiveVar(isDarkThemeVar);
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Where in the world?</h1>
-      <button type="button" className={styles.themeSwitcher} onClick={() => toggleTheme()}>
-        {isDarkTheme ? (
-          <FontAwesomeIcon icon={faMoonSolid} />
-        ) : (
-          <FontAwesomeIcon icon={faMoon} />
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Where in the world?</h1>
+        {!isMobile() && (
+          <button
+            type="button"
+            className={styles.themeSwitcher}
+            onClick={() => toggleTheme()}
+          >
+            {isDarkTheme ? (
+              <FontAwesomeIcon icon={faMoonSolid} />
+            ) : (
+              <FontAwesomeIcon icon={faMoon} />
+            )}
+            Dark Mode
+          </button>
         )}
-        Dark Mode
-      </button>
-    </div>
+      </div>
+      {isMobile() && (
+        <button
+          type="button"
+          className={styles.themeSwitcher}
+          onClick={() => toggleTheme()}
+        >
+          {isDarkTheme ? (
+            <FontAwesomeIcon icon={faMoonSolid} />
+          ) : (
+            <FontAwesomeIcon icon={faMoon} />
+          )}
+        </button>
+      )}
+    </>
   );
 }
 
